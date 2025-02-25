293d0a5f59aaee7ab8d6e81fb8a91f47b706ceac
ryufuta <rfutago@gmail.com>
2025-02-25 18:09:06 +0900

    Exclude email from API response instead of modifying model instance

--- app/controllers/api/liking_users_controller.rb
+++ app/controllers/api/liking_users_controller.rb
@@ -3,17 +3,8 @@
 class Api::LikingUsersController < ApplicationController
   def index
     blog = Blog.find(params[:blog_id])
-    @users = remove_email(blog.liking_users.order(:id))
+    @users = blog.liking_users.order(:id)
     my_liking = blog.likings.find_by(user: current_user)
     @destroy_path = my_liking ? api_blog_liking_path(blog, my_liking, format: :json) : nil
-  end
-
-  private
-
-  def remove_email(users)
-    users.map do |user|
-      user.email = nil
-      user
-    end
   end
 end
